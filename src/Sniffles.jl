9a36499c0f8df3953eea62463725c42d3976cf0e
kreikrei <kreitonstrs@gmail.com>
2021-02-18 21:26:31 +0700

    export functions

--- src/Sniffles.jl
+++ src/Sniffles.jl
@@ -10,4 +10,32 @@ include("struct.jl")
 include("settings.jl")
 include("base.jl")
 
+#struct.jl
+export vtx
+export veh
+export dt
+export col
+export dval
+export β
+export bound
+export stabilizer
+export node
+
+#base.jl
+export b
+export extract!
+export initStab
+export root
+
+#settings.jl
+export set_optimizer!
+export get_optimizer
+export reset_optimizer
+export set_slack_coeff!
+export set_surp_coeff!
+export su_C
+export sl_C
+export silence!
+export silent
+
 end
