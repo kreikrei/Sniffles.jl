494d9dc5e5860b9800c4ea8fe0b86db8a4903a5f
kreikrei <kreitonstrs@gmail.com>
2021-03-01 03:22:54 +0700

    add master

--- src/core.jl
+++ src/core.jl
@@ -27,16 +27,16 @@ function column(n::node)
             sum(l[j,i] for j in K(k).cover) - sum(l[i,j] for j in K(k).cover) == u[i] - v[i]
         ) #vehicle load balance
 
-        @constraint(sp, [i = K(k).cover], u[i] <= b().K[k].Q * y[i]) #UY
-        @constraint(sp, [i = K(k).cover], v[i] <= b().K[k].Q * z[i]) #VZ
-        @constraint(sp, [i = K(k).cover, j = K(k).cover], l[i,j] <= b().K[k].Q * x[i,j]) #XL
+        @constraint(sp, [i = K(k).cover], u[i] <= K(k).Q * y[i]) #UY
+        @constraint(sp, [i = K(k).cover], v[i] <= K(k).Q * z[i]) #VZ
+        @constraint(sp, [i = K(k).cover, j = K(k).cover], l[i,j] <= K(k).Q * x[i,j]) #XL
 
         @constraint(sp, sum(z[i] for i in K(k).cover) <= 1) #one start
 
         #SUBPROBLEM MODIFICATIONS
         F = Dict(1:length(n.bounds) .=> n.bounds)
-        uB = filter(f -> last(f).type == ">=" && F[j].S.k == k && F[j].S.t == t, F)
-        lB = filter(f -> last(f).type == "=<" && F[j].S.k == k && F[j].S.t == t, F)
+        uB = filter(f -> last(f).type == "<=" && F[j].S.k == k && F[j].S.t == t, F)
+        lB = filter(f -> last(f).type == ">=" && F[j].S.k == k && F[j].S.t == t, F)
 
         @variable(sp, g[keys(uB)], Bin)
         @variable(sp, h[keys(lB)], Bin)
@@ -75,11 +75,98 @@ function column(n::node)
 end
 
 function master(n::node)
+    mp = Model(get_optimizer())
+    if silent()
+        set_silent(mp)
+    else
+        unset_silent(mp)
+    end
+
+    R = Dict(1:length(n.columns) .=> n.columns)
+
+    # ================================
+    #    MODEL CONSTRUCTION
+    # ================================
+    @variable(mp, θ[keys(R), K(), T()] >= 0)
+    @variable(mp, I[V(), vcat(first(T()) - 1, T())])
+    @variable(mp, 0 <= slack[i = V(), t = T()] <= n.stab.slLim[i,t])
+    @variable(mp, 0 <= surp[i = V(), t = T()] <= n.stab.suLim[i,t])
+
+    @objective(mp, Min,
+        sum(
+            θ[r,k,t] * (
+                sum(
+                    dist(i,j) * (
+                        K(k).vx * R[r][(k,t)].x[i,j] +
+                        K(k).vl * R[r][(k,t)].l[i,j]
+                    )
+                    for i in K(k).cover, j in K(k).cover
+                ) +
+                sum(
+                    K(k).fd * R[r][(k,t)].u[i]
+                    for i in K(k).cover
+                ) +
+                sum(
+                    K(k).fp * R[r][(k,t)].z[i]
+                    for i in K(k).cover
+                )
+            )
+            for r in keys(R), k in K(), t in T()
+        ) + #column costs
+        sum(
+            V(i).h * I[i,t]
+            for i in V(), t in T()
+        ) + #inventory costs
+        sum(
+            n.stab.slCoeff * slack[i,t]
+            for i in V(), t in T()
+        ) - #stabilizer
+        sum(
+            n.stab.suCoeff * surp[i,t]
+            for i in V(), t in T()
+        ) #stabilizer
+    )
+
+    @constraint(mp, λ[i = V(), t = T()],
+        I[i,t - 1] +
+        sum(R[r][(k,t)].u[i] * θ[r,k,t] for r in keys(R), k in passes(i)) +
+        slack[i,t] - surp[i,t] ==
+        sum(R[r][(k,t)].v[i] * θ[r,k,t] for r in keys(R), k in passes(i)) +
+        d(i,t) + I[i,t]
+    )
+
+    @constraint(mp, δ[k = K(), i = K(k).cover, t = T()],
+        sum(R[r][(k,t)].z[i] * θ[r,k,t] for r in keys(R)) <= K(k).BP[i]
+    )
+
+    @constraint(mp, ϵ[k = K(), t = T()],
+        sum(θ[r,k,t] for r in keys(R)) <= sum(K(k).BP[i] for i in K(k).cover)
+    )
+
+    @constraint(mp, [i = V(), t = T()],
+        V(i).MIN <= I[i,t] <= V(i).MAX
+    )
+
+    @constraint(mp, [i = V()],
+        I[i,first(T())-1] == V(i).START
+    )
+
+    # ================================
+    #    BOUND GENERATOR
+    # ================================
+    F = Dict(1:length(n.bounds) .=> n.bounds)
+    uB = filter(f -> last(f).sense == "<=",F)
+    lB = filter(f -> last(f).sense == ">=",F)
+
+    @constraint(mp, ρ[j = keys(uB)], sum(θ[q.r,q.k,q.t] for q in Q(F[j].S,R)) <= F[j].κ)
+    @constraint(mp, σ[j = keys(lB)], sum(θ[q.r,q.k,q.t] for q in Q(F[j].S,R)) >= F[j].κ)
+
+    optimize!(mp)
 
     return mp
 end
 
 function sub(n::node,duals::dv)
-    
+
     return sp
 end
