462c00cc8c02c769589d3e48a678da56b0921bd5
kreikrei <kreitonstrs@gmail.com>
2021-02-28 23:21:11 +0700

    add core functions

--- src/core.jl
+++ src/core.jl
@@ -1,3 +1,23 @@
 # =========================================================================
 #    CORE FUNCTIONS AND MECHANISMS
 # =========================================================================
+
+function master(n::node)
+
+    return mp
+end
+
+function sub(n::node,duals::dv)
+
+    return sp
+end
+
+function column(n::node)
+    G = Dict{Tuple,Model}()
+
+    for k in K(), t in T()
+
+    end
+
+    return G
+end
